e22ac3cdaae0d66b529a8d18c0ef256ca058ee8e
Vinícius Seixas <vinicius.oliveira@ccc.ufcg.edu.br>
2021-03-30 17:22:55 -0300

    crud sample - https://www.digitalocean.com/community/tutorials/react-crud-context-hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import { GlobalProvider } from './context/GlobalState';
+
+import { Home } from './components/Home';
+import { AddEmployee } from './components/AddEmployee';
+import { EditEmployee } from './components/EditEmployee';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <GlobalProvider>
+      <div className="App">
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/add" component={AddEmployee} exact />
+          <Route path="/edit/:id" component={EditEmployee} exact />
+        </Switch>
+      </div>
+    </GlobalProvider>
   );
 }
 
