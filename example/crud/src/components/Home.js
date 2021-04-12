5d363dee3335f9d3f883070dce9467c09e02fc21
Vinícius Seixas <vinicius.oliveira@ccc.ufcg.edu.br>
2021-04-11 21:10:35 -0300

    refactored file structure

--- example/crud/src/components/Home.js
+++ example/crud/src/components/Home.js
@@ -8,7 +8,7 @@ export const Home = () => {
     <React.Fragment>
       <div className="container mx-auto">
         <h3 className="text-center text-3xl mt-20 text-base leading-8 text-black font-bold tracking-wide uppercase">
-          Example CRUD
+          Employees
         </h3>
         <Heading />
         <EmployeeList />
